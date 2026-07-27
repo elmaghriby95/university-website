14e6ea56bc35a53753d80b0db4339eb153fadff6
ayman elmaghriby <a.elmaghriby2022@gmail.com>
2026-07-27 20:43:11 +0200

    020

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -12,7 +12,10 @@ class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        //
+        $helpers = app_path('helpers.php');
+        if (is_file($helpers)) {
+            require_once $helpers;
+        }
     }
 
     public function boot(): void
